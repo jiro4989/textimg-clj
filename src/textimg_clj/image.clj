101cd286199073f008cd768d9a8cc6e9bec7f653
jiro4989 <jiroron666@gmail.com>
2019-07-27 18:52:22 +0900

    :heavy_plus_sign: kibit追加

--- src/textimg_clj/image.clj
+++ src/textimg_clj/image.clj
@@ -1,1 +1,6 @@
-(ns textimg-clj.image)
+(ns textimg-clj.image
+  (:import [java.awt Font]))
+
+; ファイルからフォントを読み込む http://aidiary.hatenablog.com/entry/20070408/1251466316
+
+(def font-file "/usr/share/fonts/truetype/ricty-diminished/RictyDiminished-Regular.ttf")
