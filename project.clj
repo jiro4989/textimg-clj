8f873fb5837acac2c9375cce70cdeb9009d6e74d
jiro4989 <jiroron666@gmail.com>
2019-07-27 19:47:03 +0900

    :construction: 一旦コミット

--- project.clj
+++ project.clj
@@ -1,9 +1,23 @@
 (defproject textimg-clj "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/jiro4989/textimg-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :repl-options {:init-ns textimg-clj.core}
+  :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
+  :main textimg-clj.core
+  :target-path "target/%s"
+  :native-image {:graal-bin :env/GRAALVM_HOME
+                 :opts ["--verbose"
+                        "--report-unsupported-elements-at-runtime"
+                        "--initialize-at-build-time"]
+                 :name "textimg"}
+  :profiles {:dev {:global-vars {*warn-on-reflection* true
+                                 *assert* true}}
+             :uberjar {:aot :all
+                       :native-image {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]
+                                      :graal-bin :env/GRAALVM_HOME}}}
   :plugins [[lein-kibit "0.1.7"]
-            [lein-cljfmt "0.6.4"]])
+            [lein-cljfmt "0.6.4"]
+            [io.taylorwood/lein-native-image "0.3.1"]])
